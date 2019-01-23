9d1b9ddd13caa405325850996117a8cbd398f843
thomas <iam@tangkunyin.com>
2019-01-23 16:22:49 +0800

    add last change log func
    Signed-off-by: thomas <iam@tangkunyin.com>

--- gits.sh
+++ gits.sh
@@ -7,8 +7,27 @@ echo "pm：git push origin master"
 echo "lu：git log --author username"
 echo "rp：git remote prune origin"
 echo "dcr：git branch -D (remote branch of current name)"
-echo "bsp git branch --set-upstream-to=origin/<branch> branch"
+echo "bsp: git branch --set-upstream-to=origin/<branch> branch"
+echo "gll: show the file last change info(default parameter is * )"
 echo "========== Press any key to continue ================== "
+
+function showLastChanges() {
+    if [[ "*" == $1 ]]; then
+        # show all of the files in current dir.
+       for file in ./*
+       do
+            if test -f $file; then
+                showLastChanges $file
+            fi
+       done 
+    elif [ -f $1 ]; then
+        # if single file input. then show the file last change info.
+        echo $1 | git log --pretty=format:"%an, %cd, %s [hash: %h]" -1 $1
+    else
+        echo "file not exist. check your input ${1}"
+    fi
+
+}
 
 
 current_branch=$(git symbolic-ref --short -q HEAD)
@@ -32,4 +51,8 @@ elif [[ ${choose} == "dcr" ]]; then
     git push origin :${current_branch} 
 elif [[ ${choose} == "bsp" ]]; then
     git branch --set-upstream-to=origin/${current_branch} ${current_branch} 
+elif [[ ${choose} == "gll"  ]]; then
+    echo "input fileName of *: "
+    read fileName
+    showLastChanges $fileName ? $fileName : "*"
 fi
